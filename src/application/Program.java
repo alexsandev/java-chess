781445fcd8f3ed43d9c0e9dec1cb3cf9c89a72b8
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2022-09-24 15:30:12 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Position position = new Position(5, 10);
+        System.out.println(position);
     }
 }
