3071eca43061c9924ee1ebab8a3243e08bcccd7b
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2022-10-09 12:35:44 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -28,6 +29,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen(){
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc){
         try{
             String s = sc.nextLine();
@@ -35,8 +41,8 @@ public class UI {
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         }
-        catch (RuntimeException ex){
-            throw new ChessException("Error reading ChessPosition. Valid values are from a1 to h8.");
+        catch (RuntimeException e){
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
     }
 
