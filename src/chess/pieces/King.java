afa2e6609845f3c5a354f6b3bac8ac06ce1a59e2
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2022-11-05 18:43:38 -0300

    Special move - Castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,17 +2,26 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece{
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     private boolean canMove(Position position){
         return !getBoard().thereIsAPiece(position) || isThereOpponentPiece(position);
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -70,6 +79,28 @@ public class King extends ChessPiece{
             mat[p.getRow()][p.getColumn()] = true;
         }
 
+        // #specialmove castling:
+        if(getMoveCount() == 0 && !chessMatch.getCheck()){
+            Position p1 = new Position(0, 0);
+            Position p2 = new Position(0, 0);
+            Position p3 = new Position(0, 0);
+            Position rookPosition = new Position(0, 0);
+            // #specialmove castling kingside rook:
+            p1.setValues(position.getRow(), position.getColumn() + 1);
+            p2.setValues(position.getRow(), position.getColumn() + 2);
+            rookPosition.setValues(position.getRow(), position.getColumn() + 3);
+            if(!getBoard().thereIsAPiece(p1) && !getBoard().thereIsAPiece(p2) && testRookCastling(rookPosition)){
+                mat[p2.getRow()][p2.getColumn()] = true;
+            }
+            // #specialmove castling queenside rook:
+            p1.setValues(position.getRow(), position.getColumn() - 1);
+            p2.setValues(position.getRow(), position.getColumn() - 2);
+            p3.setValues(position.getRow(), position.getColumn() - 3);
+            rookPosition.setValues(position.getRow(), position.getColumn() - 4);
+            if(!getBoard().thereIsAPiece(p1) && !getBoard().thereIsAPiece(p2) && !getBoard().thereIsAPiece(p3) && testRookCastling(rookPosition)){
+                mat[p2.getRow()][p2.getColumn()] = true;
+            }
+        }
         return mat;
     }
 }
